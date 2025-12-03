7a5ecd481efab616a838c670490401d2aff16524
Lucas <lzhang29@knox.nsw.edu.au>
2025-12-03 20:28:30 +1100

    init commit

--- src/skills_auton.cpp
+++ src/skills_auton.cpp
@@ -8,15 +8,19 @@
 void skills_auton() {
     chassis.setPose(-48.599, -15.328, 180);
     StartIntake();
-    Matchloader.extend();
-    chassis.moveToPoint(-48.599, -48.5, 1500);
+    // Matchloader.extend();
+    chassis.moveToPoint(-48.599, -46.9, 1500);
     chassis.waitUntilDone();
 
     // Move to matchloader
     chassis.turnToHeading(270, 1000);
     Matchloader.extend();
     chassis.waitUntilDone();
-    chassis.moveToPoint(-85, -48.5,3500, {.minSpeed=80});
+    chassis.moveToPoint(-55.8, -46.9, 3000, {.maxSpeed=20});
+    chassis.turnToHeading(270, 1500);
+    pros::delay(10000);
+    // chassis.moveToPoint(-71, -48.5, 1000);
+    // chassis.moveToPoint(-85, -48.5, 2000);
 
     // Move back slightly
     chassis.moveToPoint(-50, -48.5, 1000, {.forwards=false});
@@ -31,7 +35,7 @@ void skills_auton() {
     chassis.turnToHeading(90, 1000);
     chassis.waitUntilDone();
 
-    float secondMatchloaderY = -54.7;
+    float secondMatchloaderY = -57;
 
     // Move to the front of the long goal
     chassis.moveToPoint(33, -63, 2000);
