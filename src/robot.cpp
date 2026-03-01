3066ccbe9e6f67461bdaed0377b048dd7054b20a
ehu29 <ehu3001@gmail.com>
2026-03-01 13:16:16 +1100

    reset lateral pid

--- src/robot.cpp
+++ src/robot.cpp
@@ -35,14 +35,14 @@ lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
 
 
 // lateral PID controller
-lemlib::ControllerSettings lateral_controller(14.25, // proportional gain (kP)
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              125, // derivative gain (kD)
-                                              3.6, // anti windup
-                                              1, // small error range, in inches
-                                              100, // small error range timeout, in milliseconds
-                                              5, // large error range, in inches
-                                              250, // large error range timeout, in milliseconds
+                                              3, // derivative gain (kD)
+                                              0, // anti windup
+                                              0, // small error range, in inches
+                                              0, // small error range timeout, in milliseconds
+                                              0, // large error range, in inches
+                                              0, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
 
